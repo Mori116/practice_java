6d72c50129c19abb035f254d480f5d6605e27044
Mori116 <cacaomame096@gmail.com>
2021-08-24 20:23:51 +0900

    [Add]ポリモーフィズムの練習

---
+++ PracticeJava/src/polymorphism/Polymorphism01.java
@@ -0,0 +1,20 @@
+// ポリモーフィズム
+// 実行ファイル
+
+package polymorphism;
+
+import polymorphism.human.Employee;
+import polymorphism.human.Human;
+import polymorphism.human.Student;
+
+public class Polymorphism01 {
+    public static void main(String[] args) {
+        Human human1 = new Student("佐藤", 17, 70);
+        // オーバーライドしているので、Studentクラスのメソッドが実行される
+        System.out.println("Humanクラスのメソッド：プロフィールを紹介します。" + human1.getProfile());
+
+        Human human2 = new Employee("田中", 28, "システム部");
+        // オーバーライドしているので、Employeeクラスのメソッドが実行される
+        System.out.println("Humanクラスのメソッド：プロフィールを紹介します。" + human2.getProfile());
+    }
+}
